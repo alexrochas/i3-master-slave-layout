f60847fa4db5ad798c138e55f32ec33e4a02745d
Alex Rocha <alex.rochas@yahoo.com.br>
2016-10-31 13:17:28 -0200

    feat: add draft layout script

---
+++ layout.py
@@ -0,0 +1,8 @@
+import i3ipc
+
+i3 = i3ipc.Connection()
+
+# i3.command("workspace 2: Web; append_layout /home/alex/Development/i3-master-slave-layout/example.layout")
+# i3.command("workspace 4: Job")
+# i3.command("layout default")
+
